16b82c76e0aa7b7049815f1646a7e12d772f5830
suzhihui <wisdom.su@163.com>
2017-11-20 23:47:18 +0800

    changed leason 2

--- Es6/src/index.js
+++ Es6/src/index.js
@@ -13,9 +13,16 @@ console.log(a);
 })('申明方式1 => var')
 
 ;(function(){
-  // var b = "laosu"
+  var b = "laosu"
   {
     let b = "老苏"
   }
   console.log(b)
 })('申明方式 => let')
+
+;(function(){
+  for(var i =0; i< 10; i++){
+    console.log(i)
+  }
+  console.log("循环体外：" + i)
+})()
