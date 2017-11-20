246055c0360d49bdc466e9d5704b1346f435298c
suzhihui <wisdom.su@163.com>
2017-11-20 23:36:05 +0800

    add lesson 2

--- Es6/src/index.js
+++ Es6/src/index.js
@@ -1,2 +1,21 @@
 let a = 1;
 console.log(a);
+
+// var 
+// let
+// const
+
+(function(){
+  var ls = "laosu"
+  window.onload = function(){
+    console.log(ls)
+  }
+})('申明方式1 => var')
+
+;(function(){
+  // var b = "laosu"
+  {
+    let b = "老苏"
+  }
+  console.log(b)
+})('申明方式 => let')
