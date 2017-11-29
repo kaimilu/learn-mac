2aca5b0cd30b50db7adbeb3b11fe4151a67dfe72
suzhihui <wisdom.su@163.com>
2017-11-29 10:52:57 +0800

    init project

--- Es6-lottery/tasks/clean.js
+++ Es6-lottery/tasks/clean.js
@@ -1,7 +1,11 @@
 import gulp from 'gulp'
 import del from 'del'
 import args from './util/args'
+import {log,colors} from 'gulp-util'
 
 gulp.task('clean', () => {
   return del(['server/public', 'server/views'])
+    .then(paths => {
+      log('Deleted files and folders:\n',paths.join('\n'))
+    })
 })
--- Es6-lottery/tasks/scripts.js
+++ Es6-lottery/tasks/scripts.js
@@ -10,54 +10,35 @@ import rename       from 'gulp-rename'      // 文件重命名
 import uglify       from 'gulp-uglify'      // 压缩包
 import {log,colors} from 'gulp-util'        // 转出类的gulp包：日志，色彩
 import args         from './util/args'      // 命令行参数解释的包
-import { start } from 'repl';
 
 gulp.task('scripts', () => {
   return gulp.src(['app/js/index.js'])
-    pipe(plumber({
-      errorHandler:function() {
+    .pipe(plumber({
+      errorHandle: function() {
 
       }
     }))
-    // 文件重命名
     .pipe(named())
-    // 借助webpack 编译
-    // https://www.npmjs.com/package/webpack-stream
-    // .pipe(gulpWebpack({
-    //   module:{
-    //     loaders:[{
-    //       test:/\.js$/,
-    //       loader:'babel'
-    //     }]
-    //   }
-    // }),null,(err,stats)=>{
-    //   log(`Finished '${colors.cyan('scripts')}'`,stats.toString({
-    //     chunks:false
-    //   }))
-    // })
     .pipe(gulpWebpack({
-      module: {
-        loaders: [{
-          test: /\.js$/,
-          loader:'babel'
-        }]
+      module:{
+        rules: [
+          {
+            test:/\.js$/,
+            loader: 'babel-loader'
+          }
+        ]
       }
-    }, null, function(err, stats) {
-      log(`Finished '${colors.cyan('scripts')}'`, stats.toString({
-        chunks: false
+    }), null, (err,stats) => {
+      log(`Finished '${colors.cyan('scripts')}'`,stats.toString({
+        chunks:false
       }))
-    }))
-    // 文件目标路径
+    })
     .pipe(gulp.dest('server/public/js'))
-    // 重命名
     .pipe(rename({
       basename: 'cp',
       extname: '.min.js'
     }))
-    // 压缩
-    .pipe(uglify({ compress: {properties:false}, output: {'quote_keys': true}})) 
-    // 文件目标路径 到这里： 目标路径会存在一个没压缩及压缩文件： ***.cp.min.js
+    .pipe(uglify({compress:{properties:false},output:{'quote_keys':true}}))
     .pipe(gulp.dest('server/public/js'))
-    // 如果存在watch命令 就热更新
-    .pipe(gulpif(args.watch, livereload()))
+    .pipe(gulpif(args.watch,livereload()))
 })
