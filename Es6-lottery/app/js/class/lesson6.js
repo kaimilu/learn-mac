e6690f4c00498aac4b98fa96c9e601b54d0fac52
suzhihui <wisdom.su@163.com>
2017-11-30 09:51:34 +0800

    lesson6 End Array“

--- Es6-lottery/app/js/class/lesson6.js
+++ Es6-lottery/app/js/class/lesson6.js
@@ -26,3 +26,63 @@ console.log('================= 数组扩展 =================')
     })) // [2, 6, 10]
     
  }
+
+ {
+   /**
+    * https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/fill
+    * 方法用一个固定值填充一个数组中从起始索引到终止索引内的全部元素
+    * arr.fill(value, start, end)
+    * value: 用来填充数组元素的值; start :起始索引，默认值为0; end :终止索引，默认值为 this.length
+    */
+   console.log('fill-7',[1,'a',undefined].fill(7)) // fill-7 (3) [7, 7, 7]
+   console.log('fill,pos', ['a','b','c'].fill(7,1,3)) // ["a", 7, 7]
+ }
+
+ {
+   /**
+    * https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/keys
+    * keys() 方法返回一个新的Array迭代器，它包含数组中每个索引的键
+    *
+    */
+   for (let index of ['1','c','ks'].keys()) {
+     console.log('keys', index) // keys 0/1/2
+   }
+   // 有兼容问题：要index.js 里的兼容包===> babel-polyfill.js
+   for (const value of ['1,','c','ks'].values()) {
+     console.log('values', value) // values 1/c/ks
+   }
+
+   for (const [index, value] of ['1','c','ks'].entries()) {
+     console.log('entries', index, value) // entries 0 1 // 1 c // 2 ks
+   }
+ }
+
+ {
+   /**
+    * MSN: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/copyWithin
+    * copyWithin() 方法浅复制数组的一部分到同一数组中的另一个位置，并返回它，而不修改其大小
+    * arr.copyWithin(target, start, end)
+    *
+    */
+   console.log([1,2,3,4,5].copyWithin(0,3,4)) // [4, 2, 3, 4, 5]
+ }
+
+ {
+   /**
+    * https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+    * 查找 find() findIndex()
+    * find 只找到第一个就不继续找了
+    * findIndex 返回符合条件的元素下标
+    */
+    console.log("大于3的值：",[1,2,3,4,5,6].find(item => item > 3)) // 4
+    console.log("大于3的值：",[1,2,3,4,5,6].findIndex(item => item > 3)) // 3
+ }
+
+ {
+   /**
+    * https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+    */
+   // 是否包括某个元素
+   console.log('number', [1,2,NaN].includes(1)) // true
+   console.log('number', [1,2,NaN].includes(NaN)) // true
+ }
--- Es6-lottery/app/js/index.js
+++ Es6-lottery/app/js/index.js
@@ -1,4 +1,4 @@
-// import 'babel-polyfill'     // es7 兼容包
+import 'babel-polyfill'     // es7 兼容包
 import './class/lesson1.js' // 1.let const
 import './class/lesson2.js' // 2.解构赋值
 import './class/lesson3.js' // 3.正则扩展
